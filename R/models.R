1aed19993bc2957487b7b473b4ebeb9267737131
ngmaclaren <neil.g.maclaren@gmail.com>
2024-10-19 08:16:06 -0400

    Switch default models to adjacency list computation

--- R/models.R
+++ R/models.R
@@ -41,15 +41,8 @@ NULL
 #' @export
 doublewell <- function(t, x, params) {
     with(params, {
-                                        # to keep the multiplication consistent and allow for directed
-                                        # networks, form an N x N matrix in which each value of x is
-                                        # repeated in each row. These are the x_j values for each x_i.
-                                        # Use outer() to remain consistent across models.
-                                        # Using outer() with all 1s, 1s first, makes a column of x_1,
-                                        # a column of x_2, etc. Order matters for outer(). 
-                                        # Use rowSums to sum across rows (the /i/s), aka over the
-                                        # columns (the /j/s).
-        coupling <- D*rowSums(A*outer(rep(1, length(x)), x))
+        ## coupling <- D*rowSums(A*outer(rep(1, length(x)), x))
+        coupling <- D*sapply(AL, function(vs) sum(x[vs]))
         dx <- -(x - r[1])*(x - r[2])*(x - r[3]) + coupling + u
         return(list(c(dx)))
     })
@@ -70,7 +63,8 @@ doublewell <- function(t, x, params) {
 #' @export
 SIS <- function(t, x, params) {
     with(params, {
-        coupling <- D*rowSums(A*outer(1 - x, x))
+        ## coupling <- D*rowSums(A*outer(1 - x, x))
+        coupling <- D*mapply(function(xi, vs) sum((1 - xi)*x[vs]), x, AL)
         dx <- coupling - mu*x
         return(list(c(dx)))
     })
@@ -91,7 +85,8 @@ SIS <- function(t, x, params) {
 #' @export
 genereg <- function(t, x, params) {
     with(params, {
-        coupling <- D*rowSums(A*outer(rep(1, length(x)), (x^h)/(1 + (x^h))))
+        ## coupling <- D*rowSums(A*outer(rep(1, length(x)), (x^h)/(1 + (x^h))))
+        coupling <- D*sapply(AL, function(vs) sum(x[vs]^h/(1 + x[vs]^h)))
         dx <- ifelse( # handles situation where control parameter is u. If sde, use absorbing.state list in control
             x > 0,
             -B*(x^f) + coupling + u,
@@ -116,7 +111,8 @@ genereg <- function(t, x, params) {
 #' @export
 mutualistic <- function(t, x, params) {
     with(params, {
-        coupling <- D*rowSums(A*(outer(x, x)/(Dtilde + outer(E*x, H*x, `+`))))
+        ## coupling <- D*rowSums(A*(outer(x, x)/(Dtilde + outer(E*x, H*x, `+`))))
+        coupling <- D*mapply(function(xi, vs) sum((xi*x[vs])/(Dtilde + E*xi + H*x[vs])), x, AL)
         dx <- ifelse(
             x > 0,
             B + x*(1 - (x/K))*((x/C) - 1) + coupling + u,
